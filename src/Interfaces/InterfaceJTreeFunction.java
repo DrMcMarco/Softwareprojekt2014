3a0b4c7ac7381be4937243389252cc07d541d62c
DrMcMarco <DrMcMarco@users.noreply.github.com>
2014-11-25 19:26:08 +0100

    Version 0

---
+++ src/Interfaces/InterfaceJTreeFunction.java
@@ -0,0 +1,19 @@
+package Interfaces;
+
+import javax.swing.JInternalFrame;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Luca
+ */
+public interface InterfaceJTreeFunction {
+    public void openJtreeNodes(DefaultMutableTreeNode node);
+    public void setInternalFrameVisible(JInternalFrame frame);
+}
