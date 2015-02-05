1769e96e6d6edf7d4ce12eed80c2f059f6d36041
DrMcMarco <DrMcMarco@users.noreply.github.com>
2015-02-05 23:40:49 +0100

    - Kommentare

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set DERBY_HOME=%CD%\db
-call %CD%\db\bin\setNetworkServerCP.bat
-start /B CMD /C call %CD%\db\bin\startNetworkServer.bat -noSecurityManager
+set DERBY_HOME="%CD%\db"
+call "%CD%\db\bin\setNetworkServerCP.bat"
+start /B CMD /C call "%CD%\db\bin\startNetworkServer.bat" -noSecurityManager
 
-java -javaagent:Libs\openjpa-all-2.2.1.jar -cp "%CD%\Libs\beansbinding-1.2.1.jar;%CD%\Libs\derby.jar;%CD%\Libs\derbyclient.jar;%CD%\Libs\derbynet.jar;%CD%\Libs\derbyclient.jar;%CD%\Libs\openjpa-all-2.2.1.jar;%CD%\dist\Softwareprojekt2014.jar" JFrames.Start
+java -javaagent:"%CD%\Libs\openjpa-all-2.2.1.jar" -cp "%CD%\Libs\beansbinding-1.2.1.jar;%CD%\Libs\derby.jar;%CD%\Libs\derbyclient.jar;%CD%\Libs\derbynet.jar;%CD%\Libs\derbyclient.jar;%CD%\Libs\openjpa-all-2.2.1.jar;%CD%\dist\Softwareprojekt2014.jar" JFrames.Start
 
-call %CD%\db\bin\stopNetworkServer.bat
+call "%CD%\db\bin\stopNetworkServer.bat"
