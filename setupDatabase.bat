6aa21bd4dff6357213f3ff9941cab6f67c5a12d2
DrMcMarco <DrMcMarco@users.noreply.github.com>
2015-02-06 00:29:03 +0100

    - Startscript gefixt

--- setupDatabase.bat
+++ setupDatabase.bat
@@ -1,10 +1,10 @@
 @echo off
-set DERBY_HOME="%CD%\db"
-call "%CD%\db\bin\setNetworkServerCP.bat"
+set DERBY_HOME=%CD%\db
+call "%CD%"\db\bin\setNetworkServerCP.bat
 @echo "---Starte Datenbankserver---"
 @echo "---Erstelle Datenbank---"
-start /B CMD /C call "%CD%\db\bin\startNetworkServer.bat" -noSecurityManager
-call "%CD%\db\bin\ij.bat" commands.txt
+start /B CMD /C call "%CD%"\db\bin\startNetworkServer.bat -noSecurityManager
+call "%CD%"\db\bin\ij.bat commands.txt
 @echo "---Erfolg---"
 @echo "---Stoppe Datenbankserver"
-call "%CD%\db\bin\stopNetworkServer.bat"
+call "%CD%"\db\bin\stopNetworkServer.bat
--- start.bat
+++ start.bat
@@ -1,13 +1,13 @@
 @echo off
 
-set DERBY_HOME="%CD%\db"
-call "%CD%\db\bin\setNetworkServerCP.bat"
+set DERBY_HOME=%CD%\db
+call "%CD%"\db\bin\setNetworkServerCP.bat
 @echo "---Starte Datenbankserver---"
-start /B CMD /C call "%CD%\db\bin\startNetworkServer.bat" -noSecurityManager
+start /B CMD /C call "%CD%"\db\bin\startNetworkServer.bat -noSecurityManager
 
 @echo "---Starte Programm---"
 java -javaagent:"%CD%\Libs\openjpa-all-2.2.1.jar" -cp "%CD%\Libs\beansbinding-1.2.1.jar;%CD%\Libs\derby.jar;%CD%\Libs\derbyclient.jar;%CD%\Libs\derbynet.jar;%CD%\Libs\derbyclient.jar;%CD%\Libs\openjpa-all-2.2.1.jar;%CD%\dist\Softwareprojekt2014.jar" JFrames.Start
 
 @echo "---Programm beendet---"
 @echo "---Stoppe Datenbankserver---"
-call "%CD%\db\bin\stopNetworkServer.bat"
+call "%CD%"\db\bin\stopNetworkServer.bat
