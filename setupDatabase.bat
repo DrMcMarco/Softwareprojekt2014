3cb409f61bcebd3a4a1e640974b187dcbaec575f
DrMcMarco <DrMcMarco@users.noreply.github.com>
2015-02-05 16:42:59 +0100

    Datenbankserver + Libraries + Startscript

---
+++ setupDatabase.bat
@@ -0,0 +1,6 @@
+@echo off
+set DERBY_HOME=%CD%\db
+call %CD%\db\bin\setNetworkServerCP.bat
+start /B CMD /C call %CD%\db\bin\startNetworkServer.bat -noSecurityManager
+call %CD%\db\bin\ij.bat commands.txt
+call %CD%\db\bin\stopNetworkServer.bat
