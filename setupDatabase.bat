32d9c345197e9328d49a05e9b9403b99b402f65d
DrMcMarco <DrMcMarco@users.noreply.github.com>
2015-02-05 23:43:21 +0100

    - Startscript gefixt

--- setupDatabase.bat
+++ setupDatabase.bat
@@ -1,6 +1,10 @@
 @echo off
 set DERBY_HOME="%CD%\db"
 call "%CD%\db\bin\setNetworkServerCP.bat"
+@echo "---Starte Datenbankserver---"
+@echo "---Erstelle Datenbank---"
 start /B CMD /C call "%CD%\db\bin\startNetworkServer.bat" -noSecurityManager
 call "%CD%\db\bin\ij.bat" commands.txt
+@echo "---Erfolg---"
+@echo "---Stoppe Datenbankserver"
 call "%CD%\db\bin\stopNetworkServer.bat"
