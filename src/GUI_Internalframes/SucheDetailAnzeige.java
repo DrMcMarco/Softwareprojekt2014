81f396c0195333dcf4c477575bb6c07e79153d8e
simon <>
2015-02-09 13:42:06 +0100

    Suche in gp anzeigen nach gp möglich

--- src/GUI_Internalframes/SucheDetailAnzeige.java
+++ src/GUI_Internalframes/SucheDetailAnzeige.java
@@ -910,7 +910,9 @@ public class SucheDetailAnzeige extends javax.swing.JInternalFrame {
         } else if (this.hauptFenster.gibLetzteAnzeige().getTitle().equals(
                 "Geschäftspartner anlegen") || this.hauptFenster
                         .gibLetzteAnzeige().getTitle().equals("Geschäftspartner"
-                                + " ändern")) {
+                                + " ändern")
+                || this.hauptFenster.gibLetzteAnzeige().getTitle()
+                        .equals("Geschäftspartner anzeigen")) {
             //Prüfe, ob sich die Suche auf Anschrift bezieht
             if (this.tabelle.equals("Anschrift")) {
                 //Prüfe, ob ein Datensatz selektiert wurde.
@@ -924,6 +926,31 @@ public class SucheDetailAnzeige extends javax.swing.JInternalFrame {
                     //Informationen
                     this.hauptFenster.gibGeschaeftspartneranlegenFenster()
                             .setzeAnschrift(anschriftListe.get(
+                                    this.Anzeige_jTable1.getSelectedRow()));
+                    //Schließe die Suche
+                    this.setVisible(false);
+                    //Setze ggf. die Positions-Table auf nicht sichtbar
+                    this.position_jPanel1.setVisible(false);
+                    this.Positionanzeige_jTable2.setVisible(false);
+                //Wenn kein Datensatz ausgewählt wurde, wird der Benutzer
+                //Daraufhin aufmerksam gemacht
+                } else {
+                    JOptionPane.showMessageDialog(null, FEHLER_AUSWAHL, 
+                            "Fehler", JOptionPane.WARNING_MESSAGE);
+                }
+            //Gp aus der Anzeige heraus suchen
+            } else if (this.tabelle.equals("Geschäftspartner")) {
+                //Prüfe, ob ein Datensatz selektiert wurde.
+                if (this.Anzeige_jTable1.getSelectedRow() != -1) {
+                    //Caste die Ergebnisliste aus der Suche nach Anschrift.
+                    gpListe = new ArrayList<>(
+                        (Collection<? 
+                                extends Geschaeftspartner>) this.ergebnisDaten);
+                    
+                    //Setze in der View entsprechend das Feld mit den 
+                    //Informationen
+                    this.hauptFenster.gibGeschaeftspartneranlegenFenster()
+                            .zeigeGPausSucheAN(gpListe.get(
                                     this.Anzeige_jTable1.getSelectedRow()));
                     //Schließe die Suche
                     this.setVisible(false);
@@ -1018,8 +1045,6 @@ public class SucheDetailAnzeige extends javax.swing.JInternalFrame {
             }
         } else if (this.hauptFenster.gibLetzteAnzeige().getTitle().equals(
                 "Geschäftspartner ändern Einstieg") 
-                || this.hauptFenster.gibLetzteAnzeige().getTitle()
-                        .equals("Geschäftspartner anzeigen") 
                 || this.hauptFenster.gibLetzteAnzeige().getTitle()
                         .equals("Geschäftspartner anzeigen Einstieg")) {
             //Prüfe, ob sich die Suche auf Anschrift bezieht
