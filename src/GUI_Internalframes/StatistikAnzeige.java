188c6224ee8ea1a88ab50cc2ed56e52689cd0697
simon <>
2015-02-20 14:18:47 +0100

    common

--- src/GUI_Internalframes/StatistikAnzeige.java
+++ src/GUI_Internalframes/StatistikAnzeige.java
@@ -42,24 +42,11 @@ public class StatistikAnzeige extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         jToolBar1 = new javax.swing.JToolBar();
-        Zurück_jButton = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
 
         setTitle("Statistik");
 
         jToolBar1.setRollover(true);
-
-        Zurück_jButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI_Internalframes/home.PNG"))); // NOI18N
-        Zurück_jButton.setFocusable(false);
-        Zurück_jButton.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        Zurück_jButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        Zurück_jButton.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        Zurück_jButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Zurück_jButtonActionPerformed(evt);
-            }
-        });
-        jToolBar1.add(Zurück_jButton);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -93,13 +80,8 @@ public class StatistikAnzeige extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void Zurück_jButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Zurück_jButtonActionPerformed
-        this.setVisible(false);
-    }//GEN-LAST:event_Zurück_jButtonActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Zurück_jButton;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JToolBar jToolBar1;
     // End of variables declaration//GEN-END:variables
